01c690ee056a1738d9f210ae293ee91d5d6fc081
Maria Gonzalez <mayigd0502@gmail.com>
2021-12-08 20:21:01 -0500

    Update Urls

--- auth_example/views/__init__.py
+++ auth_example/views/__init__.py
@@ -1,3 +1,6 @@
 from .userCreateView  import UserCreateView
 from .userDetailView  import UserDetailView
 from .verifyTokenView import VerifyTokenView
+from .userDetailView  import UserDeleteView
+from .userDetailView  import UserUpdateView
+
--- backend_auth_example/urls.py
+++ backend_auth_example/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('verifyToken/',   views.VerifyTokenView.as_view()),
     path('user/',          views.UserCreateView.as_view()),
     path('user/<int:pk>/', views.UserDetailView.as_view()),
+    path('user/delete/<int:pk>/', views.UserDeleteView .as_view()),
+    path('user/update/<int:pk>/', views.UserUpdateView.as_view()),
 ]
